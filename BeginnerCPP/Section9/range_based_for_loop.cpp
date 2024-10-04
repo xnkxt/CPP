7b21de5feefc0b5fddbae40ad7f129f245335b70
Ankit <ankit6299659@gmail.com>
2024-10-04 20:10:45 +0530

    organized

---
+++ BeginnerCPP/Section9/range_based_for_loop.cpp
@@ -0,0 +1,14 @@
+//Range-based for Loop
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int scores [] {100,90,97};
+    for(int score : scores)
+        cout << score << endl;
+    int scores [] {100,90,97};
+    for(auto score : scores)
+        cout << score << endl;    
+}
